e0a54979e9d40c43640c960ecb3c024cb92eaa28
drewmps <andrewmarco97@gmail.com>
2025-03-10 19:02:47 +0700

    fix: mengganti foreign key di bagian asosiasi di category.js

--- models/category.js
+++ models/category.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Category.hasMany(models.Cuisine, { foreignKey: "cuisineId" });
+      Category.hasMany(models.Cuisine, { foreignKey: "categoryId" });
     }
   }
   Category.init(
