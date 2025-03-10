ef5a000d822296c870044c7871280eaae39c341e
drewmps <andrewmarco97@gmail.com>
2025-03-10 19:26:32 +0700

    feat: add seeding

---
+++ seeders/20250310095223-seed-Users.js
@@ -0,0 +1,19 @@
+"use strict";
+const fs = require("fs").promises;
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    let rows = JSON.parse(await fs.readFile("./data/users.json", "utf8"));
+    rows = rows.map((row) => {
+      delete row.id;
+      row.createdAt = new Date();
+      row.updatedAt = new Date();
+      return row;
+    });
+    await queryInterface.bulkInsert("Users", rows);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
