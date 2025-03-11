308087620a8996231142908ccf30a1fc054b0d63
drewmps <andrewmarco97@gmail.com>
2025-03-11 15:21:23 +0700

    fix: menambahkan code untuk hash password

--- seeders/20250310095223-seed-Users.js
+++ seeders/20250310095223-seed-Users.js
@@ -1,4 +1,7 @@
 "use strict";
+
+const { hashPassword } = require("../helpers/bcrypt");
+
 const fs = require("fs").promises;
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -6,6 +9,7 @@ module.exports = {
     let rows = JSON.parse(await fs.readFile("./data/users.json", "utf8"));
     rows = rows.map((row) => {
       delete row.id;
+      row.password = hashPassword(row.password);
       row.createdAt = new Date();
       row.updatedAt = new Date();
       return row;
