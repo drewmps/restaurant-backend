f2bd9f1416833dbd49e1325236eb222773390498
drewmps <andrewmarco97@gmail.com>
2025-03-12 14:41:20 +0700

    fix: memindahkan endpoint yang pub menjadi di atas middleware authentication di app.js

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.post("/login", UserController.login);
+app.get("/pub/cuisines", CuisineController.getCuisines);
+app.get("/pub/cuisines/:id", CuisineController.getCuisineById);
 
 app.use(authentication);
 app.post("/cuisines", CuisineController.createCuisine);
@@ -25,9 +27,6 @@ app.delete("/cuisines/:id", authorization, CuisineController.deleteCuisineById);
 app.post("/categories", CategoryController.createCategory);
 app.get("/categories", CategoryController.getCategories);
 app.put("/categories/:id", CategoryController.editCategoryById);
-
-app.get("/pub/cuisines", CuisineController.getCuisines);
-app.get("/pub/cuisines/:id", CuisineController.getCuisineById);
 
 app.post("/add-user", guardAdmin, UserController.addUser);
 
