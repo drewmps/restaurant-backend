0674c0e061d5a9b585167d30490be603709ac72a
drewmps <andrewmarco97@gmail.com>
2025-03-12 15:14:58 +0700

    fix: menyeragamkan semua respond error menjadi object yang ada property message dan berisi array

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -13,23 +13,23 @@ function errorHandler(err, req, res, next) {
       return;
     case "BadRequest":
       res.status(400).json({
-        message: err.message,
+        message: [err.message],
       });
       return;
     case "Unauthorized":
-      res.status(401).json({ message: err.message });
+      res.status(401).json({ message: [err.message] });
       return;
     case "Forbidden":
-      res.status(403).json({ message: err.message });
+      res.status(403).json({ message: [err.message] });
       return;
     case "NotFound":
-      res.status(404).json({ message: err.message });
+      res.status(404).json({ message: [err.message] });
       return;
     case "JsonWebTokenError":
-      res.status(401).json({ message: "Invalid token" });
+      res.status(401).json({ message: ["Invalid token"] });
       return;
     default:
-      res.status(500).json({ message: "Internal Server Error" });
+      res.status(500).json({ message: ["Internal Server Error"] });
   }
 }
 module.exports = errorHandler;
